4a9bc8f144838727839e4723870749d12ea94580
Yakovlev0309 <yakovlev.universe@gmail.com>
2024-08-16 15:33:40 +0300

    Создание динамической библиотеки

---
+++ ads_lib.cpp
@@ -0,0 +1,9 @@
+#include "ads_lib.h"
+#include <iostream>
+
+using namespace std;
+
+ADS_LIB::ADS_LIB()
+{
+    cout << "ASD_LIB object created" << endl;
+}
---
+++ ads_lib.h
@@ -0,0 +1,12 @@
+#ifndef ADS_LIB_H
+#define ADS_LIB_H
+
+#include "ADS_LIB_global.h"
+
+class ADS_LIB_EXPORT ADS_LIB
+{
+public:
+    ADS_LIB();
+};
+
+#endif // ADS_LIB_H
